182926cca07a2f3c061a30cdcb6139dda6d3d4f8
aitor <aitorscinfo@gmail.com>
2022-09-28 19:35:05 +0100

    Resolve 28-09-22.ejercicio6

---
+++ php_28-09-22/ejercicio6.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+        Realizar un programa que cada vez que se ejecute muestre tu nombre a un tamaño
+        elegido al azar entre 200% y 700%.
+    */
+
+    $random_width = rand(200,700);
+
+    echo "<h1 style='font-size: $random_width%'>Aitor</h1>"
+?>
