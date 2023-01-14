0e63eec5b22d8a405c0ba3e671e865a7e0b027fe
Sstark97 <aitorscinfo@gmail.com>
2023-01-14 08:46:58 +0000

    Split Edit student into a function

--- 12-01-23/db_functions.php
+++ 12-01-23/db_functions.php
@@ -29,4 +29,72 @@
 
         return $result;
     }
+
+    function getStudent () {
+        $userId = $_GET["userId"];
+
+        $result = [
+            "get_error_student" => "",
+        ];
+
+        try {
+            $connection = getDbConnection();
+            $sql_query = "SELECT * FROM Students WHERE id = $userId";
+    
+            $sentence = $connection->prepare($sql_query);
+            $sentence->execute();
+            $student = $sentence->fetch();
+            $result["student"] = $student;
+        } catch (PDOException $error) {
+            $result["get_error_student"] = $error->getMessage();
+        }
+
+        return $result;
+    }
+
+    function edit () {
+        $userId = $_GET["userId"];
+
+        $result = [
+            "error" => false,
+            "message" => "User Updated success!"
+        ];
+
+        try {
+            $connection = getDbConnection();
+
+            $student = [
+                "name" => $_POST["name"],
+                "surname" => $_POST["surname"],
+                "email" => $_POST["email"],
+                "age" => intval($_POST["age"]),
+                "age" => $_POST["age"],
+                "id" => $userId
+            ];
+            
+            $sql_query = <<< END
+                UPDATE Students
+                SET name=:name,
+                surname=:surname,
+                email=:email,
+                age=:age
+                WHERE id=:id
+            END;
+
+            $sentence = $connection->prepare($sql_query);
+
+            foreach($student as $key => $field) {
+                $type = $key === "id" || $key === "age" ? PDO::PARAM_INT : PDO::PARAM_STR;
+
+                $sentence->bindValue(":$key", $field, $type);
+            }
+
+            $sentence->execute();
+        } catch (PDOException $error) {
+            $result["error"] = true;
+            $result["message"] = $error->getMessage();
+        }
+
+        return $result;
+    }
 ?>
--- 12-01-23/pages/edit.php
+++ 12-01-23/pages/edit.php
@@ -1,87 +1,41 @@
 <?php
-    include "../functions.php";
+    require_once "../db_functions.php";
     require_once "../parts/parts.php";
 
-    $error = false;
-    $userId = $_GET["userId"];
-
-    if(isset($_POST["submit"])) {
-        $result = [
-            "error" => false,
-            "message" => "User Updated success!"
-        ];
-
-        try {
-            $connection = getDbConnection();
-
-            $student = [
-                "name" => $_POST["name"],
-                "surname" => $_POST["surname"],
-                "email" => $_POST["email"],
-                "age" => intval($_POST["age"]),
-                "age" => $_POST["age"],
-                "id" => $userId
-            ];
-            
-            $sql_query = <<< END
-                UPDATE Students
-                SET name=:name,
-                surname=:surname,
-                email=:email,
-                age=:age
-                WHERE id=:id
-            END;
-
-            $sentence = $connection->prepare($sql_query);
-
-            foreach($student as $key => $field) {
-                $type = $key === "id" || $key === "age" ? PDO::PARAM_INT : PDO::PARAM_STR;
-
-                $sentence->bindValue(":$key", $field, $type);
-            }
-
-            $sentence->execute();
-        } catch (PDOException $error) {
-            $result["error"] = true;
-            $result["message"] = $error->getMessage();
-        }
-    }
-
-    try {
-        $connection = getDbConnection();
-        $sql_query = "SELECT * FROM Students WHERE id = $userId";
-
-        $sentence = $connection->prepare($sql_query);
-        $sentence->execute();
-        $student = $sentence->fetch();
-    } catch (PDOException $error) {
-        $error = $error->getMessage();
-    }
+    [
+        "get_error_student" => $get_error_student,
+        "student" => $student
+    ] = getStudent();
 ?>
 
 <?= createHeader() ?>
-<?php if($error): ?>
-    <div class="container mt-2">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="alert alert-danger" role="alert">
-                    <?= $error ?>
+    <?php if(isset($_POST["submit"])): ?>
+        <?php $result = edit(); ?>
+    <?php endif; ?>
+
+    <?php if($get_error_student): ?>
+        <div class="container mt-2">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="alert alert-danger" role="alert">
+                        <?= $error ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-<?php endif; ?>
-<?php if(isset($result)): ?>
-    <div class="container mt-3">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="alert alert-<?= $result['error'] ? 'danger' : 'success' ?>" role="alert">
-                    <?= $result["message"] ?>
+    <?php endif; ?>
+
+    <?php if(isset($result)): ?>
+        <div class="container mt-3">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="alert alert-<?= $result['error'] ? 'danger' : 'success' ?>" role="alert">
+                        <?= $result["message"] ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-<?php endif; ?>
+    <?php endif; ?>
     <div class="container">
     <div class="row">
         <div class="col-md-12">
@@ -89,7 +43,7 @@
         </div>
         <hr>
         <?php if(!$student): ?>
-            <p class="text-center">No existe el usuario con el id <?= $userId ?></p>
+            <p class="text-center">No existe el usuario con el id <?= $_GET["userId"] ?></p>
         <?php else: ?>
         <form method="post" action="<?= $_SERVER['PHP_SELF']?>?userId=<?= $student["id"] ?>">
             <div class="form-group">
