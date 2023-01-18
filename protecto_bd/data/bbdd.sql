a8d871196386f8ca2845fe96ff68bb6a5261b73c
aitor <aitorscinfo@gmail.com>
2023-01-18 19:17:34 +0000

    Add DB and Login

---
+++ protecto_bd/data/bbdd.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS GameShop;
+USE GameShop;
+
+GRANT ALL PRIVILEGES ON GameShop.* TO aitor97@'%' IDENTIFIED BY '12345';
+FLUSH PRIVILEGES;
+
+CREATE TABLE User (
+    dni VARCHAR(9) PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    surname VARCHAR(75) NOT NULL,
+    email VARCHAR(65) NOT NULL,
+    password VARCHAR(70) NOT NULL,
+    phone VARCHAR(9),
+    age INT NOT NULL ,
+    is_admin BOOLEAN DEFAULT false
+);
+
+CREATE TABLE VideoGame(
+    id INT PRIMARY KEY,
+    name VARCHAR(75) NOT NULL,
+    description VARCHAR(350) NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    price DOUBLE NOT NULL,
+    assesment DOUBLE,
+    release_date DATE
+);
+
+CREATE TABLE WhisList(
+    dni VARCHAR(9) NOT NULL,
+    gameId INT NOT NULL,
+    PRIMARY KEY (dni, gameId)
+);
+
+ALTER TABLE WhisList ADD FOREIGN KEY (dni) REFERENCES User(dni);
+ALTER TABLE WhisList ADD FOREIGN KEY (gameId) REFERENCES VideoGame(id); 
--- protecto_bd/index.php
+++ protecto_bd/index.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    require_once "./partials/parts.php";
+    session_name("videogames");
+    session_start();
+
+    $userId = $_SESSION["userId"];
+
+    if(!isset($userId)) {
+        header("Location: ./pages/login.php");
+    }
+?>
+
+<?= createHeader() ?>
+    <h1>Hola</h1>
+<?= createFooter() ?>
