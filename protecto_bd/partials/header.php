16b12e676811e9d80faa412c7c5bcb4531bfdf6f
Sstark97 <aitorscinfo@gmail.com>
2023-01-21 23:36:23 +0000

    Add path controll

--- protecto_bd/partials/header.php
+++ protecto_bd/partials/header.php
@@ -1,7 +1,3 @@
-<?php 
-    $path = strpos(__DIR__, "") !== false ? "../" : "";
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -21,10 +17,10 @@
 
 
     <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="<?= $path ?>assets/css/fontawesome.css">
-    <link rel="stylesheet" href="<?= $path ?>assets/css/templatemo-cyborg-gaming.css">
-    <link rel="stylesheet" href="<?= $path ?>assets/css/owl.css">
-    <link rel="stylesheet" href="<?= $path ?>assets/css/animate.css">
+    <link rel="stylesheet" href="../assets/css/fontawesome.css">
+    <link rel="stylesheet" href="../assets/css/templatemo-cyborg-gaming.css">
+    <link rel="stylesheet" href="../assets/css/owl.css">
+    <link rel="stylesheet" href="../assets/css/animate.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
 </head>
