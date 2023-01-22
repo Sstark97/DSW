ae2d002f028b7ec06ff9be942676f01b0ccef935
Sstark97 <aitorscinfo@gmail.com>
2023-01-22 15:43:10 +0000

    Comprobe that a game exist when edit

--- protecto_bd/partials/gameForm.php
+++ protecto_bd/partials/gameForm.php
@@ -18,42 +18,47 @@
     $action = empty($id) ? $_SERVER['PHP_SELF'] : $_SERVER['PHP_SELF'] . "?id=$id";
 ?>
 
-<!-- Formulario de Creación/Edición de VideoJuegos -->
-<div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h2 class="mt-4"><?= $title ?> Videojuego</h2>
+<!-- Si no existe el juego mostramos un mensaje -->
+<?php if(!empty($id) && !$game): ?>
+    <h2 class="text-center">No existe el videojuego con el id <?= $id ?> </h2>
+<?php else: ?>
+    <!-- Formulario de Creación/Edición de VideoJuegos -->
+    <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-4"><?= $title ?> Videojuego</h2>
+                </div>
+                <hr>
+                <form class="text-white" method="post" action="<?= $action ?>">
+                    <div class="form-group">
+                        <label for="game[name]">Nombre</label>
+                        <input type="text" name="game[name]" class="form-control" value="<?= $game["name"] ?? "" ?>" >
+                    </div>
+                    <div class="form-group">
+                        <label for="game[description]">Descripción</label>
+                        <textarea name="game[description]"  rows="8" class="form-control"><?= $game["description"] ?? "" ?></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="game[genre]">Género</label>
+                        <input type="text" name="game[genre]" class="form-control" value="<?= $game["genre"] ?? "" ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="game[price]">Precio</label>
+                        <input type="number" step=".01" name="game[price]" class="form-control" value="<?= $game["price"] ?? "4.99" ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="game[assesment]">Popularidad</label>
+                        <input type="number" step=".01" name="game[assesment]" class="form-control" value="<?= $game["assesment"] ?? "" ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="game[release_date]">Fecha de lanzamiento</label>
+                        <input type="date" name="game[release_date]" class="form-control" value="<?= $game["release_date"] ?? "" ?>">
+                    </div>
+                    <div class="form-group mt-3">
+                        <button type="submit" name="submit[add]" class="btn btn-primary">Enviar</button>
+                        <a class="btn btn-primary" href="../index.php">Regresar al inicio</a>
+                    </div>
+                </form>
             </div>
-            <hr>
-            <form class="text-white" method="post" action="<?= $action ?>">
-                <div class="form-group">
-                    <label for="game[name]">Nombre</label>
-                    <input type="text" name="game[name]" class="form-control" value="<?= $game["name"] ?? "" ?>" >
-                </div>
-                <div class="form-group">
-                    <label for="game[description]">Descripción</label>
-                    <textarea name="game[description]"  rows="8" class="form-control"><?= $game["description"] ?? "" ?></textarea>
-                </div>
-                <div class="form-group">
-                    <label for="game[genre]">Género</label>
-                    <input type="text" name="game[genre]" class="form-control" value="<?= $game["genre"] ?? "" ?>">
-                </div>
-                <div class="form-group">
-                    <label for="game[price]">Precio</label>
-                    <input type="number" step=".01" name="game[price]" class="form-control" value="<?= $game["price"] ?? "4.99" ?>">
-                </div>
-                <div class="form-group">
-                    <label for="game[assesment]">Popularidad</label>
-                    <input type="number" step=".01" name="game[assesment]" class="form-control" value="<?= $game["assesment"] ?? "" ?>">
-                </div>
-                <div class="form-group">
-                    <label for="game[release_date]">Fecha de lanzamiento</label>
-                    <input type="date" name="game[release_date]" class="form-control" value="<?= $game["release_date"] ?? "" ?>">
-                </div>
-                <div class="form-group mt-3">
-                    <button type="submit" name="submit[add]" class="btn btn-primary">Enviar</button>
-                    <a class="btn btn-primary" href="../index.php">Regresar al inicio</a>
-                </div>
-            </form>
         </div>
-    </div>
+<?php endif; ?>
