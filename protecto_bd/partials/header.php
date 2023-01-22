55b018ddb78f32e0e1bec30f881b44dff4d7b985
Sstark97 <aitorscinfo@gmail.com>
2023-01-22 22:44:53 +0000

    Add new favicon

--- protecto_bd/partials/header.php
+++ protecto_bd/partials/header.php
@@ -18,7 +18,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
 
-    <title>Cyborg - Awesome HTML5 Template</title>
+    <link rel="shortcut icon" href="<?= $path ?>assets/images/favicon.ico" type="image/x-icon">
+
+    <title>Cyborg GameShop</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
